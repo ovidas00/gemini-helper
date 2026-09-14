1a4776f10a1af494eb1ed88f63c2d9f95ae7c781
ovidas00 <designerovi00@gmail.com>
2026-09-14 13:24:49 +0600

    updated

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
+import { GeminiService } from 'src/chat/gemini.service';
 
 @Controller('webhook')
 export class WebhookController {
-  constructor(private readonly webhookService: WebhookService) {}
+  constructor(
+    private readonly webhookService: WebhookService,
+    private readonly geminiService: GeminiService,
+  ) {}
 
   @Get('facebook')
   verifyFacebookWebhook(
@@ -19,7 +23,19 @@ export class WebhookController {
   }
 
   @Post('facebook')
-  handleFacebookWebhook(@Body() data) {
-    console.log(data);
+  async handleFacebookWebhook(@Body() data: any) {
+    const message = data.entry?.[0]?.messaging?.[0]?.message?.text;
+
+    if (!message) {
+      return {
+        success: true,
+        message: 'No message text found',
+      };
+    }
+
+    const response = await this.geminiService.chat(message);
+    console.log(response);
+
+    return 'done';
   }
 }
