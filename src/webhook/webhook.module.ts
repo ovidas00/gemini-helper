18cb02e5d0082088fabe36ff2a7cc734ec1334ae
ovidas00 <designerovi00@gmail.com>
2026-09-14 13:26:08 +0600

    updated

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
+import { GeminiService } from 'src/chat/gemini.service';
 
-@Module({ controllers: [WebhookController], providers: [WebhookService] })
+@Module({
+  controllers: [WebhookController],
+  providers: [WebhookService, GeminiService],
+})
 export class WebhookModule {}
